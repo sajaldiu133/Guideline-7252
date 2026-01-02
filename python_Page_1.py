a29d4d77b0142e4d3de18295c05a78456c3ae878
sajaldiu133 <sajaldiu133@gmail.com>
2026-01-02 20:11:21 +0600

    Thats file is enough.

---
+++ python_Page_1.py
@@ -0,0 +1,97 @@
+#Basic code of python (page 1 )
+
+#If you know basic C, this is for you.
+
+
+"""     comment  er mainly 2 type 
+                              1. singale line - # (use korte hoi line er suru te )  
+                              2. Multiline -     pura ta ekta multiline comment e likha 
+
+"""
+
+
+#topic = 1 (simple print)
+
+
+#data type: 
+
+""" amra  mainly 4 ta nea kaj kori
+
+1. int 
+2. float
+3. str 
+4. bool """
+
+
+""" simple print er jnno amra use korbo   {print} . Tobe string print er jnno double or single 
+
+Quotation  mark use korbo """
+
+
+#simple print
+
+#problem 1 (Hello World!)
+
+print("Hello World!") #double Quotation mark.
+
+print('Hello World!') #singale quotation mark.
+
+
+
+#topic = 2 (variable)
+
+      #print er step
+
+"""  1. variable assin 
+     2.print(variable)
+"""
+
+
+
+                             # Arithmetic oparetion    
+                             # (   + , - , * , / , %    )
+                             
+                             
+                             
+#problem2 (simple operation and variable declar)
+
+a = 10     #variable assin   (value assin korar jnno {  =  } Assignment Operator)                         
+b = 5
+print(a) # a print 
+
+print(b) #b print
+
+                                         #sum      1.  direct print function e 
+                               #         2.  alada variable o nite pari
+
+c = a + b
+print(c)
+
+#or 
+print(a+b)
+
+
+                                                      
+ 
+
+#problem2.1 
+a = 10 
+b = 5
+c = a +b 
+print(c)  #note sting print e only quotation use korbo
+
+                                  # others oparetion same as Sum 
+                                  # Note: %(vagses/remainder)
+
+#problem 2.2
+#Reminder or Vagses ber kora 
+
+ 
+x = 19
+y = 4
+
+print(x % y)
+
+                 #output = 3
+                 
+                                                       #end
